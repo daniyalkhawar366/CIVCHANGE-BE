deb5643713d7523824114b9a95598ea663532054
daniyalkhawar366 <daniyalkhawar3@gmail.com>
2025-07-06 17:03:39 +0500

    port fix

--- server/index.js
+++ server/index.js
@@ -299,7 +299,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
